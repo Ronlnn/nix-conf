3f563a3adba96ad1f386cede5755f1c77953c235
roninn <artking01kg@gmail.com>
2025-09-11 15:44:33 +0300

    -message

--- sys-modules/gnome.nix
+++ sys-modules/gnome.nix
@@ -8,4 +8,10 @@
     services.xserver.desktopManager.gnome.enable = true;
     # Выключаем дефолтные программы
     services.gnome.core-apps.enable = true;
+
+    services.xserver = {
+      xkb.layout = "us,ru";
+      xkbVariant = "";
+      xkbOptions = "grp:win_space_toggle";
+  };
 }
