40646f74adcd0c6ba861f459dce6de11f58c30aa
roninn <artking01kg@gmail.com>
2025-09-11 15:25:29 +0300

    -message

--- sys-modules/gnome.nix
+++ sys-modules/gnome.nix
@@ -8,9 +8,11 @@
     # Включение gnome
     services.xserver.desktopManager.gnome.enable = true;
 
+    services.xserver.enable = true;
     # Клавиатура
     services.xserver.xkb = {
       layout = "us,ru";
+      variant = "";
       options = "grp:alt_shift_toggle";
     };
 
