d062f9cb918f3d0fd27af4a4d283865a2851415d
roninn <artking01kg@gmail.com>
2025-09-14 20:21:34 +0300

    lang

--- sys-modules/language.nix
+++ sys-modules/language.nix
@@ -4,7 +4,7 @@
   time.timeZone = "Europe/Moscow";
 
  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.defaultLocale = "ru_RU.UTF-8";
   i18n.supportedLocales = ["en_US.UTF-8/UTF-8" "ru_RU.UTF-8/UTF-8"];
 
   i18n.extraLocaleSettings = {
